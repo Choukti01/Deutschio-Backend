529c55add291547f92210120bd606d0d9642f484
Choukti01 <achoukti1999@gmail.com>
2025-12-25 18:55:31 +0100

    adding changing the UI from email to name

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
+    console.error('MongoDB connection error:', err.message);
   });
+
 
 // User Schema
 const userSchema = new mongoose.Schema({
