169236d049ffd6ce6c6d390b828e5e7962daf561
Choukti01 <achoukti1999@gmail.com>
2025-12-26 06:31:14 +0100

    updates in cors

--- server.js
+++ server.js
@@ -7,23 +7,17 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());          
 app.use(express.json());
 
+app.use(cors({
+  origin: 'https://ddeutschio.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ddeutschio.netlify.app');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
 
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 
 
 
